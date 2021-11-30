c91b157868d797a350c7dfe308757e3c7a4e2ee9
nico_devanthery <devantherynicolas@gmail.com>
2021-11-30 15:42:23 +0100

    commit interface graphique

---
+++ WebApp/Controllers/RestaurantController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class RestaurantController : Controller
+    {
+
+
+        public static List<Models.Restaurant> myRestos = new List<Models.Restaurant>
+        {
+            new Models.Restaurant {ID_RESTAURANT = 1 , IDCITY =5 , IDTYPE =1 , NAME="jet pizza",ADRESS="", PHONENUMBER ="01234" , USERNAME = "jetPizza" , PASSWORD="askfdn"},
+            new Models.Restaurant {ID_RESTAURANT = 2 , IDCITY =4 , IDTYPE =5 , NAME="jet za",ADRESS="", PHONENUMBER ="567" , USERNAME = "saifnasf" , PASSWORD="askfdn"},
+            new Models.Restaurant {ID_RESTAURANT = 3 , IDCITY =3 , IDTYPE =4 , NAME="holy cow",ADRESS="", PHONENUMBER ="89" , USERNAME = "jsafneaf" , PASSWORD="askfdn"},
+            new Models.Restaurant {ID_RESTAURANT = 4 , IDCITY =2 , IDTYPE =3 , NAME="jet",ADRESS="", PHONENUMBER ="1" , USERNAME = "jeifewfeetPizza" , PASSWORD="askfdn"},
+            new Models.Restaurant {ID_RESTAURANT = 5 , IDCITY =1 , IDTYPE =1 , NAME="piza",ADRESS="", PHONENUMBER ="28412348" , USERNAME = "eaifjw c " , PASSWORD="askfdn"},
+
+        };
+
+        public IActionResult Index()
+        {
+            return View(myRestos);
+        }
+    }
+}
---
+++ WebApp/Models/Restaurant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Restaurant
+    {
+        public int ID_RESTAURANT { get; set; }
+
+        public int IDCITY { get; set; }
+
+        public int IDTYPE { get; set; }
+
+        public string NAME { get; set; }
+
+        public string ADRESS { get; set; }
+
+        public string PHONENUMBER { get; set; }
+
+        public string USERNAME { get; set; }
+
+        public string PASSWORD { get; set; }
+
+        public override string ToString()
+        {
+            return "ID RESTAURANT : " + ID_RESTAURANT +
+                   "ID CITY : " + IDCITY +
+                   "ID TYPE : " + IDTYPE +
+                   "NAME : " + NAME +
+                   "ADRESS : " + ADRESS +
+                   "PHONE NUMBER : " + PHONENUMBER+
+                   "USERNAME : " + USERNAME +
+                   "PASSWORD : " + PASSWORD 
+                   ;
+
+        }
+
+
+    }
+}
